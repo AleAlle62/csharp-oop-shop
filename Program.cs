4271bff7382269cecc862b9d5845f94d9b41ad1c
AleAlle62 <basketallegrini@gmail.com>
2023-03-21 10:13:00 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿var p = new Product("Ogetto")
+{
+    Price = 100,
+    Iva = 0.22,
+    Description = "bo non lo so blabla",
+};
+
+Console.WriteLine(p);
+Console.WriteLine($"price after iva: {p.GetPriceAfterIva()}; extended name: {p.GetExtendedName()}.");
